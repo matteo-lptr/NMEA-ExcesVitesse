e6a84c2a9793d0bfa9396aa8a697d314b3e5f872
T3KTON1K <mattz.layz@gmail.com>
2024-11-08 08:57:11 +0100

    Ajout du fichier python

---
+++ main.py
@@ -0,0 +1,4 @@
+# type de trame | heure d'envoi | latitude Nord | longitude Ouest | positionnement | nombre de sat | horizontale   | Altitude | Hauteur du géoïde | Vide DGPS |  Somme de contrôle
+#  GPGGA,         075404.00,     4817.50121,N,    00201.26375,W,      1,            05,                2.85,         93.2,M,         48.1,M,                      ,*78
+
+
